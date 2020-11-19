06e88d052220efc7f829b288bdf4438dc4490de5
LiAn Song <songlian7@gmail.com>
2020-11-18 18:42:39 -0700

    add methods that respond to i want something and i something you

--- src/Magpie4.java
+++ src/Magpie4.java
@@ -48,6 +48,20 @@ public class Magpie4 {
 			response = transformIWantToStatement(statement);
 		}
 
+		else if (findKeyword(statement,"I want", 0) >= 0){
+			response = transformIWantStatement(statement);
+		}
+
+		else if (findKeyword(statement, "I", 0) >= 0){
+			int psn = findKeyword(statement, "I", 0);
+
+			if (psn >= 0 && findKeyword(statement, "you", psn) >= 0) {
+				response = transformIYouStatement(statement);
+			} else {
+				response = getRandomResponse();
+			}
+		}
+
 		else {
 			// Look for a two word (you <something> me)
 			// pattern
@@ -82,6 +96,18 @@ public class Magpie4 {
 		return "What would it mean to " + restOfStatement + "?";
 	}
 
+	private String transformIWantStatement(String statement) {
+		// Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
+
 	/**
 	 * Take a statement with "you <something> me" and transform it into
 	 * "What makes you think that I <something> you?"
@@ -104,6 +130,21 @@ public class Magpie4 {
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe)
 				.trim();
 		return "What makes you think that I " + restOfStatement + " you?";
+	}
+
+	private String transformIYouStatement(String statement) {
+		// Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfI = findKeyword(statement, "I", 0);
+		int psnOfYou = findKeyword(statement, "you", psnOfI + 1);
+
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+		return "Why do you " + restOfStatement + " me?";
 	}
 
 	/**
